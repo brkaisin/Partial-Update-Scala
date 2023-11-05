71305332a76a962850147fe359f964a975f7f71f
Brieuc Kaisin <brieuc.kaisin@b12-consuting.com>
2023-11-05 21:50:52 +0100

    improved typing in type class derivation

--- src/main/scala-2.13/be/brkaisin/partialupdate/core/PartialUpdator.scala
+++ src/main/scala-2.13/be/brkaisin/partialupdate/core/PartialUpdator.scala
@@ -6,68 +6,69 @@ import magnolia1._
 import scala.language.experimental.macros
 
 /**
-  * [[PartialUpdator]] provides the ability to update a value of type T with a [[Partial]].
-  * @tparam P the type of the [[Partial]]. The reason why the type of the complete value
-  * is not declared as a type parameter is because magnolia does not support type-class derivation
-  * with more than one type parameter.
+  * [[PartialUpdator]] provides the ability to partially update a value of type C with a [[Partial]].
+  * The reason why the type of the complete value is not declared as a type parameter is because magnolia
+  * does not support type-class derivation with more than one type parameter. It is also impossible to state
+  * that P <:< Partial[C] in the type class definition because of magnolia limitations.
+  * @tparam P the type of the [[Partial]].
   */
 trait PartialUpdator[P] {
 
   /**
     * Update a value of type T with a [[Partial]].
     * @param partial the [[Partial]] to apply
-    * @param currentComplete the value to update
-    * @tparam C the type of the value to update
+    * @param currentComplete the value to partially update
+    * @tparam C the type of the value to partially update
     * @return the updated value
     */
-  def updated[C](partial: P, currentComplete: C): C
+  def updated[C](partial: P, currentComplete: C)(implicit ev: P <:< Partial[C]): C
 
 }
 
 object PartialUpdator {
   type Typeclass[P] = PartialUpdator[P]
+  private def partialUpdate[C, P <: Partial[C]](partial: P, currentComplete: C): C =
+    partial.toCompleteUpdated(currentComplete)
 
   implicit def partialFieldUpdator[T]: PartialUpdator[PartialField[T]] =
     new PartialUpdator[PartialField[T]] {
-      def updated[C](partial: PartialField[T], currentComplete: C): C =
-        // C is of type T
-        partial.toCompleteUpdated(currentComplete.asInstanceOf[T]).asInstanceOf[C]
+      def updated[C](partial: PartialField[T], currentComplete: C)(implicit ev: PartialField[T] <:< Partial[C]): C =
+        partialUpdate(ev(partial), currentComplete)
     }
 
   implicit def partialNestedFieldUpdator[T, PartialFieldType <: Partial[T]]
       : PartialUpdator[PartialNestedField[T, PartialFieldType]] =
     new PartialUpdator[PartialNestedField[T, PartialFieldType]] {
-      def updated[C](partial: PartialNestedField[T, PartialFieldType], currentComplete: C): C =
-        // C is of type T
-        partial.toCompleteUpdated(currentComplete.asInstanceOf[T]).asInstanceOf[C]
+      def updated[C](partial: PartialNestedField[T, PartialFieldType], currentComplete: C)(implicit
+          ev: PartialNestedField[T, PartialFieldType] <:< Partial[C]
+      ): C = partialUpdate(ev(partial), currentComplete)
     }
 
   implicit def partialOptionalFieldUpdator[T, PartialFieldType <: Partial[T]]
       : PartialUpdator[PartialOptionalField[T, PartialFieldType]] =
     new PartialUpdator[PartialOptionalField[T, PartialFieldType]] {
-      def updated[C](partial: PartialOptionalField[T, PartialFieldType], currentComplete: C): C =
-        // C is of type Option[T]
-        partial.toCompleteUpdated(currentComplete.asInstanceOf[Option[T]]).asInstanceOf[C]
+      def updated[C](partial: PartialOptionalField[T, PartialFieldType], currentComplete: C)(implicit
+          ev: PartialOptionalField[T, PartialFieldType] <:< Partial[C]
+      ): C = partialUpdate(ev(partial), currentComplete)
     }
 
   implicit def partialListFieldUpdator[Id, T <: Identifiable[T, Id], PartialFieldType <: Partial[T]]
       : PartialUpdator[PartialListField[Id, T, PartialFieldType]] =
     new PartialUpdator[PartialListField[Id, T, PartialFieldType]] {
-      def updated[C](partial: PartialListField[Id, T, PartialFieldType], currentComplete: C): C =
-        // C is of type List[T]
-        partial.toCompleteUpdated(currentComplete.asInstanceOf[List[T]]).asInstanceOf[C]
+      def updated[C](partial: PartialListField[Id, T, PartialFieldType], currentComplete: C)(implicit
+          ev: PartialListField[Id, T, PartialFieldType] <:< Partial[C]
+      ): C = partialUpdate(ev(partial), currentComplete)
     }
 
   // Implementation of join is not pure but modifies the current instance by reflection instead of creating a new
   // one. This is the only way found because magnolia does not support type-class derivation with more than one type
-  // parameter. Here, we do not construct an instance of [[P]] but we rather use the instance of [[P]] to update the
-  // current instance of [[C]], for which we do not have a "rawConstruct" method. For purity in your code, the argument
-  // "currentComplete" of method [[PartialUpdator.updated]] must be a copy of the current instance.
+  // parameter. Here, we do not construct an instance of P but we rather use the instance of P to partially update
+  // the current instance of C in place because we do not have a "rawConstruct" method for C. For purity in your code,
+  // the argument "currentComplete" of method "PartialUpdator.updated" must be a copy of the current instance.
   def join[P](caseClass: CaseClass[Typeclass, P]): Typeclass[P] =
     new PartialUpdator[P] {
-      def updated[C](partial: P, currentComplete: C): C =
+      def updated[C](partial: P, currentComplete: C)(implicit ev: P <:< Partial[C]): C =
         caseClass.parameters.foldLeft(currentComplete) { (currentCompleteAcc, param) =>
-          // copy currentComplete to avoid modifying it
           val paramValuePartial = param.dereference(partial)
 
           // Using reflection to get and set the value in the complete object
@@ -75,7 +76,9 @@ object PartialUpdator {
           fieldComplete.setAccessible(true) // because the field is "private final" in the case class
           val paramValueComplete = fieldComplete.get(currentCompleteAcc)
 
-          val completedValue = param.typeclass.updated(paramValuePartial, paramValueComplete)
+          // dummy proof that PType is a Partial[AnyRef], which is a necessary condition for the type class derivation to work
+          implicit val dummy: param.PType <:< Partial[AnyRef] = ev.asInstanceOf[param.PType <:< Partial[AnyRef]]
+          val completedValue                                  = param.typeclass.updated(paramValuePartial, paramValueComplete)
 
           fieldComplete.set(currentCompleteAcc, completedValue)
           currentCompleteAcc
@@ -85,4 +88,5 @@ object PartialUpdator {
   implicit def gen[P]: Typeclass[P] = macro Magnolia.gen[P]
 
   def apply[P](implicit updator: PartialUpdator[P]): PartialUpdator[P] = updator
+
 }
